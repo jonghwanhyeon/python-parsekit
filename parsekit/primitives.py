f80034836c802fa736fb6608ba5e9bde6ce72280
Jonghwan Hyeon <hyeon0145@gmail.com>
2022-12-22 20:44:56 +0900

    Consume whitespaces automatically

--- parsekit/primitives.py
+++ parsekit/primitives.py
@@ -2,10 +2,22 @@ import re
 
 from parsekit import Failure, Parser, Success
 
+whitespace_pattern = re.compile(r"[ \t]+")
+
+
+def consume_whitespace(stream, pos):
+    match = whitespace_pattern.match(stream, pos)
+    if match is None:
+        return pos
+
+    return match.end(0)
+
 
 def literal(text):
     @Parser
     def literal_parser(stream, pos):
+        pos = consume_whitespace(stream, pos)
+
         if stream.startswith(text, pos):
             return Success(pos, pos + len(text), text)
         else:
@@ -19,6 +31,8 @@ def regex(pattern, flags=0):
 
     @Parser
     def regex_parser(stream, pos):
+        pos = consume_whitespace(stream, pos)
+
         match = compiled.match(stream, pos)
         if match:
             return Success(pos, match.end(0), match.group(0))
